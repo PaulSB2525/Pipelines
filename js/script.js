16386caafd4a8d7a1aa8713895b7d2fb4c438b2e
PaulSB2525 <90229268+PaulSB2525@users.noreply.github.com>
2026-02-11 08:07:20 -0600

    Add new character to the database
    Added a new character 'epstein' with a corresponding image.

--- js/script.js
+++ js/script.js
@@ -2,12 +2,14 @@
 const db = {
   mario: "Habilidad: Salto Pro. Mundo: Mushroom Kingdom.",
   link: "Habilidad: Maestro de la Espada. Mundo: Hyrule.",
-  samus: "Habilidad: Disparo de Plasma. Mundo: Zebes."
+  samus: "Habilidad: Disparo de Plasma. Mundo: Zebes.",
+  epstein: "Habilidad: Fiestas. Mundo: Little St. James"
 };
 
 const btnSearch = document.getElementById('btnSearch');
 const inputField = document.getElementById('characterInput');
 const resultArea = document.getElementById('resultArea');
+const resultPic = document.getElementById('resultPic');
 
 btnSearch.addEventListener('click', () => {
   const query = inputField.value.toLowerCase().trim();
@@ -18,5 +20,8 @@ btnSearch.addEventListener('click', () => {
   } else {
     resultArea.innerHTML = `<p>Error 404: Personaje no encontrado en la base de datos.</p>`;
     resultArea.style.borderColor = "#e94560"; // Rojo si falla
+  }
+  if(db[query]=="epstein"){
+    resultPic.src = "https://media.vanityfair.com/photos/5d63ff8db5890000082acd12/master/w_2560%2Cc_limit/Epstein-October-2019-Lede.jpg";
   }
 });
